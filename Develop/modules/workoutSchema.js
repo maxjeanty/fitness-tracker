cf38e3591201e997a2d250f0b8046811df23f1a5
maxjeanty <maxjeanty@gmail.com>
2020-11-12 21:58:29 -0600

    initial push
    initial push..

---
+++ Develop/modules/workoutSchema.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+// const date = require('dateformat')
+
+const WorkoutSchema = new Schema({
+  exercises: [],
+  date: Date,
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Workout = mongoose.model('Workout', WorkoutSchema);
+
+module.exports = Workout;
---
+++ Develop/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+require('./db/mongoose');
+
+const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+// const collections = ["workoutSeeds"];
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static('public'));
+
+//ROUTES;
+
+require('./routes/html_routes.js')(app);
+require('./routes/api_routes.js')(app);
+
+app.listen(PORT, () => {
+  console.log(`App running on port ${PORT}!`);
+});
