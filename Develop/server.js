9850a75371905944bc505fbc2ddc8b55458e0ba3
maxjeanty <maxjeanty@gmail.com>
2020-11-13 22:34:30 -0600

    fixed deploy url

--- Develop/server.js
+++ Develop/server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/deep-thoughts',
+  process.env.MONGODB_URI || 'mongodb://localhost/max1',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
